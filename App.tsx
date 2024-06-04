e4122a65237433272409598b031064a5467b5d5d
Boroviina@protonmail.com <117476003+Boroviina@users.noreply.github.com>
2024-06-04 16:47:02 +0200

    Client added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+import LoginScreen from './screens/LoginScreen';
+import StartScreen from "./screens/StartScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import {useState} from "react";
+import ReportProblemScreen from "./screens/ReportProblemScreen";
+import Reviosion from "./screens/Reviosion";
+import {AuthInit, AuthProvider, useAuth} from "./shared/contexts/auth-context";
+
+const Stack = createNativeStackNavigator();
+
+
+function AuthentificationScreens() {
+    const {auth} = useAuth();
+
+
+    return (<Stack.Navigator>
+            {auth ? <>
+                <Stack.Screen name={'Revision'} component={Reviosion}/>
+            </> : <>
+                <Stack.Screen name='StartScreen' component={StartScreen}/>
+                <Stack.Screen name='Login' component={LoginScreen}/>
+                <Stack.Screen name='SignUp' component={SignUpScreen}/>
+                <Stack.Screen name={'ReportProblem'} component={ReportProblemScreen}/>
+            </>}
+        </Stack.Navigator>
+    )
+}
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <AuthProvider>
+            <AuthInit>
+                <NavigationContainer>
+                    <AuthentificationScreens/>
+                </NavigationContainer>
+            </AuthInit>
+        </AuthProvider>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
